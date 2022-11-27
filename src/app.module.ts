d645788aba7280383b2a188b2b47174bd405a646
Ashish Reddy <arashishreddy7@gmail.com>
2022-11-27 21:05:25 +0530

    added graphql and finished few Queries

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: './schema.gql',
+      playground: true,
+      typePaths: ['./**/*.graphql'],
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
